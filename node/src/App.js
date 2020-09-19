c9fdeedcd5bb58d3ca921046346a3c1c769c897b
Kevin Tang <kevintang2023@gmail.com>
2020-09-19 16:56:41 -0400

    Change structure & separate gitignores

---
+++ node/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  require('dotenv').config();
+  return (
+    <div className="App">
+      <div id="header">
+        <h2>bubble</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
